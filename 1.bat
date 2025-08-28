e8e03f806d63ad5e7bd80a12ed8b387856557711
jackmusk44 <shraukihme76@gmail.com>
2025-08-28 14:42:19 +0300

    Add files via upload

--- 1.bat
+++ 1.bat
@@ -4,8 +4,11 @@ setlocal
 set "PYTHON_DIR=%USERPROFILE%\Python312"
 set "INSTALLER=%TEMP%\python-installer.exe"
 
+rem полный путь к PowerShell
+set "POWERSHELL=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
+
 if not exist "%PYTHON_DIR%\python.exe" (
-    powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest 'https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe' -OutFile '%INSTALLER%'"
+    "%POWERSHELL%" -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest 'https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe' -OutFile '%INSTALLER%'"
 
     if exist "%INSTALLER%" (
         "%INSTALLER%" /quiet InstallAllUsers=0 TargetDir="%PYTHON_DIR%" PrependPath=1 Include_pip=1 Include_test=0
